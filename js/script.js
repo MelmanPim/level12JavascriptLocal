055ad562d3e38baaf942b6f5bf8306c18c3d38b3
MelmanPim <59539663+MelmanPim@users.noreply.github.com>
2021-06-10 13:37:21 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log("Hallo Winc Academy studenten");
+
+/*
+
+// Dit is een grote som
+let multiply = 1230941 * 1823794;
+console.log(multiply);
+
+let devision = 637263 / 54;
+console.log(devision);
+
+ */
